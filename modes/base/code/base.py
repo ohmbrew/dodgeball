2d7cd591dff8060fb361ae0d2ac64ac0bca22fa1
ohm <gjbrault@gmail.com>
2023-12-05 11:56:54 -0700

    committing from pin to work on lappy

--- modes/base/code/base.py
+++ modes/base/code/base.py
@@ -6,16 +6,20 @@ import serial
 ser = serial.Serial("/dev/ttyACM0", 115200)
 
 class Base(Mode):
-    def listen(self):
-        while True:
-            if (ser.inWaiting() > 0):
-                data_str = ser.read(ser.inWaitin()).decode('ascii')
-            println(data_str, end='')
+    # def listen(self):
+    #     while True:
+    #         if (ser.inWaiting() > 0):
+    #             data_str = ser.read(ser.inWaitin()).decode('ascii')
+    #         println(data_str, end='')
     def mode_init(self):
         print("[Serial Monitor] Base Mode custom python is initialized.")
         
 
     def mode_start(self, **kwargs):
         print("[Serial Monitor] Base Mode custom python is starting.")
-        listen()
+        while True:
+            if (ser.inWaiting() > 0):
+                data_str = ser.read(ser.inWaiting()).decode('ascii')
+            print(data_str, end='')
+            
         
